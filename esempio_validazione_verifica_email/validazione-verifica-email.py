191cd6d093d81658a6a2995ac908dc34ed67a4eb
lukes1582 <lukes1582@gmail.com>
2021-02-13 08:00:44 +0100

    esempio di validazione e verifica email
    esempio di validazione e verifica email

---
+++ esempio_validazione_verifica_email/validazione-verifica-email.py
@@ -0,0 +1,13 @@
+'''
+Created on 13/02/2021
+@author: lukes158@gmail.com l0m1s
+'''
+
+from verify_email import verify_email
+from validate_mail import validate_email
+
+print("Risultato verifica email")
+print(verify_email('lukes1582@gmail.com'))
+print("-----------------------------------")
+print("Risultato validazione email")
+print(validate_email('lukes1582@gmail.com'))
